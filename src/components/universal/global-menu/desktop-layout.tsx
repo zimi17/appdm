b73b15ea1d90f479d50e5e31564b36dfa43cf5b9
Azimi Arsyad <azimi.syad@gmail.com>
2025-08-18 17:50:56 +0000

    kalau secara pengelolaan sendiri, apakah approach tersebut sudah tepat?

--- src/components/universal/global-menu/desktop-layout.tsx
+++ src/components/universal/global-menu/desktop-layout.tsx
@@ -1,14 +1,14 @@
 
 "use client";
 
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import { cn } from "@/lib/utils";
 import { MenuColumn } from "./menu-column";
 import { navLinks } from "@/lib/data/nav";
 
 export const DesktopLayout = ({ activeL1, activeL2, handleNavLinkClick }: any) => {
-  const l1Active = !!activeL1;
-  const l2Active = !!activeL2;
+  const l2Active = !!activeL1;
+  const l3Active = !!activeL2;
 
   return (
     <div className="h-full flex w-full relative overflow-hidden">
@@ -16,7 +16,7 @@ export const DesktopLayout = ({ activeL1, activeL2, handleNavLinkClick }: any) =
             className={cn(
                 "h-full shrink-0 transition-transform duration-500 ease-in-out",
                 "md:w-1/2 lg:w-[37.5%]", 
-                l2Active ? "md:-translate-x-full" : "md:translate-x-0"
+                l3Active ? "md:-translate-x-full" : "md:translate-x-0"
             )}
         >
             <MenuColumn
@@ -30,9 +30,9 @@ export const DesktopLayout = ({ activeL1, activeL2, handleNavLinkClick }: any) =
         <motion.div
             className={cn(
                 "h-full shrink-0 absolute md:relative inset-0 bg-[#292c2f] transition-transform duration-500 ease-in-out",
-                "md:w-1/2 lg:w-[31.25%]",
-                l1Active ? "translate-x-0" : "translate-x-full",
-                l2Active ? "md:-translate-x-full" : "md:translate-x-0"
+                "w-full md:w-1/2 lg:w-[31.25%]",
+                l2Active ? "translate-x-0" : "translate-x-full",
+                l3Active ? "md:-translate-x-full" : "md:translate-x-0"
             )}
         >
             {activeL1?.sublinks && (
@@ -49,8 +49,8 @@ export const DesktopLayout = ({ activeL1, activeL2, handleNavLinkClick }: any) =
         <motion.div
             className={cn(
                 "h-full shrink-0 absolute md:relative inset-0 bg-[#292c2f] transition-transform duration-500 ease-in-out",
-                "md:w-1/2 lg:w-[31.25%]",
-                l2Active ? "translate-x-0" : "translate-x-full"
+                 "w-full md:w-1/2 lg:w-[31.25%]",
+                l3Active ? "translate-x-0" : "translate-x-full"
             )}
         >
              {activeL2?.sublinks && (
