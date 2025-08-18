093edb705d71b87cc3b805297b6997fdcc80a610
Azimi Arsyad <azimi.syad@gmail.com>
2025-08-18 18:37:59 +0000

    level 2 berhasil tergeser pada perbaikan ini, tetapi level 3 tidak berge

--- src/components/universal/global-menu/desktop-layout.tsx
+++ src/components/universal/global-menu/desktop-layout.tsx
@@ -47,8 +47,7 @@ export const DesktopLayout = ({ activeL1, activeL2, handleNavLinkClick }: any) =
         className={cn(
           "h-full shrink-0 transition-transform duration-500 ease-[cubic-bezier(0.65,0,0.35,1)]",
           "w-full md:w-1/2 lg:w-[37.5%]",
-          l3Active ? "md:-translate-x-full" : "translate-x-0",
-          "lg:translate-x-0"
+          l3Active ? "md:-translate-x-full lg:translate-x-0" : "translate-x-0",
         )}
       >
         <MenuColumn
@@ -60,17 +59,17 @@ export const DesktopLayout = ({ activeL1, activeL2, handleNavLinkClick }: any) =
       </motion.div>
 
       {/* Level 2 Column */}
-      <div className={cn("h-full shrink-0 md:border-l md:border-white/15",
-        "w-full md:w-1/2 lg:w-[31.25%]",
-        "absolute md:relative inset-0 md:inset-auto",
-        "transition-transform duration-500 ease-[cubic-bezier(0.65,0,0.35,1)]",
-        l2Active ? "translate-x-0" : "translate-x-full",
-        l3Active ? "md:-translate-x-full lg:translate-x-0" : "md:translate-x-0",
-        !l2Active && "hidden"
-      )}>
+      <motion.div
+        className={cn(
+            "h-full shrink-0 w-full md:w-1/2 lg:w-[31.25%]",
+            "transition-transform duration-500 ease-[cubic-bezier(0.65,0,0.35,1)]",
+            l3Active ? "md:-translate-x-full lg:translate-x-0" : "md:translate-x-0",
+            l2Active ? "flex" : "hidden"
+        )}
+      >
         <AnimatePresence>
           {l2Active && (
-            <motion.div {...animationProps} className="h-full w-full">
+            <motion.div {...animationProps} className="h-full w-full md:border-l md:border-white/15">
               <MenuColumn
                 links={activeL1?.sublinks || []}
                 onLinkClick={handleNavLinkClick}
@@ -81,19 +80,16 @@ export const DesktopLayout = ({ activeL1, activeL2, handleNavLinkClick }: any) =
             </motion.div>
           )}
         </AnimatePresence>
-      </div>
+      </motion.div>
 
       {/* Level 3 Column */}
-       <div className={cn("h-full shrink-0 md:border-l md:border-white/15",
-        "w-full md:w-1/2 lg:w-[31.25%]",
-        "absolute md:relative inset-0 md:inset-auto",
-        "transition-transform duration-500 ease-[cubic-bezier(0.65,0,0.35,1)]",
-        l3Active ? "translate-x-0" : "translate-x-full",
-        !l3Active && "hidden"
-      )}>
+       <motion.div className={cn(
+        "h-full shrink-0 w-full md:w-1/2 lg:w-[31.25%]",
+        l3Active ? "flex" : "hidden"
+       )}>
          <AnimatePresence>
           {l3Active && (
-            <motion.div {...animationProps} className="h-full w-full">
+            <motion.div {...animationProps} className="h-full w-full md:border-l md:border-white/15">
               <MenuColumn
                 links={activeL2?.sublinks || []}
                 onLinkClick={handleNavLinkClick}
@@ -104,7 +100,7 @@ export const DesktopLayout = ({ activeL1, activeL2, handleNavLinkClick }: any) =
             </motion.div>
           )}
         </AnimatePresence>
-      </div>
+      </motion.div>
     </div>
   );
 };
