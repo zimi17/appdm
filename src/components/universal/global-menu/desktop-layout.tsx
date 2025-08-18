cacbb6c56cd928c88854cd228e0f98e6eac7b0e7
Azimi Arsyad <azimi.syad@gmail.com>
2025-08-18 18:08:08 +0000

    mungkin untuk animate presence pada level 2 dan level 3 kita bisa gunaka

--- src/components/universal/global-menu/desktop-layout.tsx
+++ src/components/universal/global-menu/desktop-layout.tsx
@@ -1,14 +1,51 @@
 
 "use client";
 
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { cn } from "@/lib/utils";
 import { MenuColumn } from "./menu-column";
 import { navLinks } from "@/lib/data/nav";
 
+const l2Variants = {
+    hidden: { opacity: 0, x: "100%" },
+    visible: { opacity: 1, x: 0 },
+    exit: { opacity: 0, x: "100%" },
+};
+
+const l3Variants = {
+    hidden: { opacity: 0, x: "100%" },
+    visible: { opacity: 1, x: 0 },
+    exit: { opacity: 0, x: "100%" },
+};
+
+
 export const DesktopLayout = ({ activeL1, activeL2, handleNavLinkClick }: any) => {
   const l2Active = !!activeL1;
   const l3Active = !!activeL2;
+
+  const animationProps = {
+    transition:{
+        duration: 0.6,
+        ease: [0.65, 0, 0.35, 1],
+        opacity: {
+        duration: 0.2,
+        ease: [0.65, 0, 0.35, 1]
+        }
+    }
+  }
+
+  const exitAnimationProps = {
+    transition: {
+        duration: 0.6,
+        ease: [0.65, 0, 0.35, 1],
+        opacity: {
+        duration: 0.2,
+        ease: [0.65, 0, 0.35, 1],
+        delay: 0.4 
+        }
+    }
+  }
+
 
   return (
     <div className="h-full flex w-full relative overflow-hidden">
@@ -16,7 +53,7 @@ export const DesktopLayout = ({ activeL1, activeL2, handleNavLinkClick }: any) =
             className={cn(
                 "h-full shrink-0 transition-transform duration-300 ease-in-out",
                 "w-full md:w-1/2 lg:w-[37.5%]", 
-                l3Active ? "md:-translate-x-full lg:translate-x-0" : "md:translate-x-0"
+                l3Active && "md:-translate-x-full lg:translate-x-0"
             )}
         >
             <MenuColumn
@@ -33,6 +70,9 @@ export const DesktopLayout = ({ activeL1, activeL2, handleNavLinkClick }: any) =
                 "w-full md:w-1/2 lg:w-[31.25%]",
                 l2Active ? "translate-x-0" : "translate-x-full"
             )}
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: l2Active ? 1 : 0, y: l2Active ? 0 : -10, ...animationProps }}
+            exit={{ opacity: 0, y: -10, ...exitAnimationProps }}
         >
              <div className={cn(!l2Active && "hidden", "w-full h-full")}>
                 {activeL1?.sublinks && (
@@ -53,6 +93,9 @@ export const DesktopLayout = ({ activeL1, activeL2, handleNavLinkClick }: any) =
                 "w-full md:w-1/2 lg:w-[31.25%]",
                 l3Active ? "translate-x-0" : "translate-x-full"
             )}
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: l3Active ? 1 : 0, y: l3Active ? 0 : -10, ...animationProps }}
+            exit={{ opacity: 0, y: -10, ...exitAnimationProps}}
         >
             <div className={cn(!l3Active && "hidden", "w-full h-full")}>
              {activeL2?.sublinks && (
--- src/components/universal/global-menu/mobile-layout.tsx
+++ src/components/universal/global-menu/mobile-layout.tsx
@@ -5,6 +5,29 @@ import { motion, AnimatePresence } from "framer-motion";
 import { MenuColumn } from "./menu-column";
 import { navLinks } from "@/lib/data/nav";
 
+const animationProps = {
+    transition:{
+        duration: 0.6,
+        ease: [0.65, 0, 0.35, 1],
+        opacity: {
+        duration: 0.2,
+        ease: [0.65, 0, 0.35, 1]
+        }
+    }
+  }
+
+const exitAnimationProps = {
+    transition: {
+        duration: 0.6,
+        ease: [0.65, 0, 0.35, 1],
+        opacity: {
+        duration: 0.2,
+        ease: [0.65, 0, 0.35, 1],
+        delay: 0.4 
+        }
+    }
+}
+
 export const MobileLayout = ({ activeL1, activeL2, handleNavLinkClick }: any) => (
     <div className="h-full w-full relative overflow-hidden">
         <motion.div
@@ -27,9 +50,9 @@ export const MobileLayout = ({ activeL1, activeL2, handleNavLinkClick }: any) =>
                 <motion.div
                     key="l2"
                     className="w-full h-full absolute inset-0 bg-[#292c2f]"
-                    initial={{x: '100%'}}
-                    animate={{x: activeL2 ? '-100%' : '0'}}
-                    exit={{x: '100%'}}
+                    initial={{ opacity: 0, y: -10 }}
+                    animate={{ opacity: 1, y: 0, ...animationProps }}
+                    exit={{ opacity: 0, y: -10, ...exitAnimationProps }}
                     transition={{duration: 0.3, ease: 'easeInOut'}}
                 >
                     <MenuColumn
@@ -48,9 +71,9 @@ export const MobileLayout = ({ activeL1, activeL2, handleNavLinkClick }: any) =>
                  <motion.div
                     key="l3"
                     className="w-full h-full absolute inset-0 bg-[#292c2f]"
-                    initial={{x: '100%'}}
-                    animate={{x: 0}}
-                    exit={{x: '100%'}}
+                    initial={{ opacity: 0, y: -10 }}
+                    animate={{ opacity: 1, y: 0, ...animationProps }}
+                    exit={{ opacity: 0, y: -10, ...exitAnimationProps }}
                     transition={{duration: 0.3, ease: 'easeInOut'}}
                 >
                      <MenuColumn
