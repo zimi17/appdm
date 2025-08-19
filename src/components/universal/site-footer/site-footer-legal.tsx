08d8094d75eb8e615de2fe7fabc925e5fdf83c04
Azimi Arsyad <azimi.syad@gmail.com>
2025-08-19 02:45:01 +0000

    Hilangkan kata "Hak Cipta" karena sudah ada simbol copyright, mungkin le

--- src/components/universal/site-footer/site-footer-legal.tsx
+++ src/components/universal/site-footer/site-footer-legal.tsx
@@ -16,10 +16,10 @@ export function SiteFooterLegal({ items = [], copyright}: SiteFooterLegalProps) 
     return (
         <div className="bg-background/80">
             <div className="container mx-auto px-6 flex flex-col md:flex-row justify-between items-center py-4 text-sm text-muted-foreground">
-                <p className="order-2 md:order-1 mt-4 md:mt-0">
-                    Hak Cipta © {currentYear} {copyright || "STIE Dwimulya"}
+                <p className="order-2 md:order-1 mt-4 md:mt-0 text-center md:text-left">
+                    © {currentYear} {copyright || "Yayasan Dwimulya Sahati dan Civitas Akademika STIE Dwimulya."}
                 </p>
-                <div className="flex flex-wrap justify-center gap-x-4 gap-y-2 order-1 md:order-2">
+                <div className="flex flex-wrap justify-center md:justify-start gap-x-4 gap-y-2 order-1 md:order-2">
                     {items.map((link, i) => (
                         <Link key={i} href={link.href} className="hover:text-primary transition-colors">{link.children}</Link>
                     ))}
--- src/components/universal/site-footer/site-footer.tsx
+++ src/components/universal/site-footer/site-footer.tsx
@@ -57,7 +57,7 @@ export function SiteFooter() {
           </div>
         </div>
       </div>
-      <SiteFooterLegal items={footerLegalItems} copyright="Yayasan Dwimulya Sahati" />
+      <SiteFooterLegal items={footerLegalItems} copyright="Yayasan Dwimulya Sahati dan Civitas Akademika STIE Dwimulya." />
     </footer>
   );
 }
