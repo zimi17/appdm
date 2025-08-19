87dddbbc8c649c68cba094322ecd167cb0394de4
Azimi Arsyad <azimi.syad@gmail.com>
2025-08-19 05:25:54 +0000

    saya tidak tahu harus berkata apa lagi

--- src/components/universal/site-footer/site-footer-legal.tsx
+++ src/components/universal/site-footer/site-footer-legal.tsx
@@ -16,11 +16,11 @@ export function SiteFooterLegal({ items = [], copyright }: SiteFooterLegalProps)
   return (
     <div className="bg-secondary text-secondary-foreground">
       <div className="max-w-screen-2xl mx-auto px-6 py-4 border-t border-white/10 text-sm text-muted-foreground">
-        <div className="grid grid-cols-1 md:grid-cols-16 gap-y-4 items-baseline gap-x-8">
-            <div className="md:col-span-4 text-center md:text-left">
+        <div className="grid grid-cols-1 lg:grid-cols-16 gap-y-4 items-baseline gap-x-8">
+            <div className="lg:col-span-4 text-center md:text-left">
                 <p>{copyrightText}</p>
             </div>
-            <div className="md:col-span-12 flex flex-wrap justify-center md:justify-start gap-x-4 gap-y-2">
+            <div className="lg:col-span-12 flex flex-wrap justify-center md:justify-start gap-x-4 gap-y-2">
                 {items.map((link, i) => (
                 <Link key={i} href={link.href} className="hover:text-white transition-colors">
                     {link.children}
